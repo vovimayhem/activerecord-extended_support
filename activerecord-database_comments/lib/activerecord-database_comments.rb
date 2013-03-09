8ccde404f6234966e7d5bbcbe6055923ab7b1e9a
Roberto Quintanilla <roberto.quintanilla@naranya.com>
2013-03-09 10:45:26 -0600

    UPdate de require de activerecord-database_schema

--- activerecord-database_comments/lib/activerecord-database_comments.rb
+++ activerecord-database_comments/lib/activerecord-database_comments.rb
@@ -1,2 +1,3 @@
 # encoding: utf-8
+require 'activerecord-database_schema'
 require 'active_record/database_comments/railtie' if defined?(Rails::Railtie)
--- activerecord-database_unsigned_columns/lib/activerecord-database_unsigned_columns.rb
+++ activerecord-database_unsigned_columns/lib/activerecord-database_unsigned_columns.rb
@@ -1,2 +1,3 @@
 # encoding: utf-8
+require 'activerecord-database_schema'
 require 'active_record/database_unsigned_columns/railtie' if defined?(Rails::Railtie)
