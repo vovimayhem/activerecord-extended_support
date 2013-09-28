96688a118e202231683ccba083a9a221f6301162
Roberto Quintanilla <Vov@Vovi-Mayhems-iMac.local>
2013-09-28 12:15:15 -0500

    Correccion de comments - se estaba probando contra una version no liberada de rails4

--- activerecord-database_comments/lib/active_record/version_four/comments/connection_adapters.rb
+++ activerecord-database_comments/lib/active_record/version_four/comments/connection_adapters.rb
@@ -68,7 +68,7 @@ module ActiveRecord
             sql = "SHOW FULL FIELDS FROM #{quote_table_name(table_name)}"
             execute_and_free(sql, 'SCHEMA') do |result|
               each_hash(result).map do |field|
-                field_name = set_field_encoding(field[:Field])
+                # field_name = set_field_encoding(field[:Field])
                 new_column(field_name, field[:Default], field[:Type], field[:Null] == "YES", field[:Collation], field[:Extra], field[:Comment])
               end
             end
